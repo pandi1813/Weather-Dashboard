b4f3ec6ddc66e47043aa994e7a9aca06f3df2077
pandi1813 <andrea.peter1813@gmail.com>
2023-01-29 10:28:49 +0000

    displayed the first element from local storage as search history button

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
 let searchButton = document.querySelector("#search-button");
-let cityInput
+let cityInput;
+let searchHistory = document.querySelector("#history")
 
 
 
-
+displaySearchHistory(JSON.parse(localStorage.getItem("cityList"))[0])
 
 searchButton.addEventListener("click", function(event){
     
@@ -12,34 +13,35 @@ searchButton.addEventListener("click", function(event){
     cityInput = document.querySelector("#search-input").value
     if (cityInput !== "") {
         //if there city input is not empty, fetch the geo details for the requested city
-
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=5&appid=872873b89a0afdc97c82762a115e655a`)
-    .then(response => response.json())
-    .then(citiesFound => {
-        let firstCity = citiesFound[0];
-        console.log(firstCity.lat);
-        console.log(firstCity.lon)
-    
-        //using the geo details fetch the weather forecast 
-        return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=872873b89a0afdc97c82762a115e655a`);
-
-    })
-    
-    .then(response => response.json())
-    .then(data => {
         
-        console.log(data)
-    })
-
-    saveCityHistory()
-    
-}
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=5&appid=872873b89a0afdc97c82762a115e655a`)
+        .then(response => response.json())
+        .then(citiesFound => {
+            let firstCity = citiesFound[0];
+            console.log(firstCity.lat);
+            console.log(firstCity.lon)
+            
+            //using the geo details fetch the weather forecast 
+            return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=872873b89a0afdc97c82762a115e655a`);
+            
+        })
+        
+        .then(response => response.json())
+        .then(data => {
+            
+            console.log(data)
+        })
+        
+        saveCityHistory()
+        
+    }
 })
 
 
 
 //*******************  FUNCTIONS  ***************************
 
+//save search history in local storage
 function saveCityHistory() {
     
         let cityHistory = JSON.parse(localStorage.getItem("cityList")) || [];
@@ -49,4 +51,11 @@ function saveCityHistory() {
         localStorage.setItem("cityList", JSON.stringify(cityHistory));
         console.log("list: "+ localStorage.getItem("cityList"))
     
+}
+
+//display buttons for search history
+function displaySearchHistory (cityName) {
+    let historyButton = document.createElement("button");
+    historyButton.innerHTML = cityName;
+    searchHistory.prepend(historyButton)
 }
