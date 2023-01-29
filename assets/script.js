4c95d0b6886653347d5da4f6fcc1b37037c889b5
pandi1813 <andrea.peter1813@gmail.com>
2023-01-29 19:03:55 +0000

    created function to display forecast and added a bootstrap card to display details

--- assets/script.js
+++ assets/script.js
@@ -102,12 +102,13 @@ function displaySearchHistory () {
 
 
 // display current weather
-// console.log(currentDate)
+
 
 // currentWeather()
 function currentWeather(name, icon, temperature, wind, humidity) {
     // console.log(today)
-    currentWeatherSection.innerHTML = `<div class="jumbotron jumbotron-fluid">
+    currentWeatherSection.innerHTML = 
+   `<div class="jumbotron jumbotron-fluid">
     <div class="container">
       <h1 class= "display-4">${name}</h1>
       <h2 class="display-5">${currentDate}<img src = http://openweathermap.org/img/wn/${icon}@2x.png ></h2>
@@ -118,7 +119,21 @@ function currentWeather(name, icon, temperature, wind, humidity) {
   </div>`
 }
 
+
+// display cards for weather forecast
+
 function displayForecast() {
-    console.log("test")
-    
+    forecastSection.innerHTML = "";
+
+    console.log("test");
+    let forecastCard = document.createElement("div");
+    forecastCard.innerHTML =
+   `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+  </div>`;
+  forecastSection.append(forecastCard);
 }
